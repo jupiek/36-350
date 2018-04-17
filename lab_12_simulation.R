60304c98e2dd45d5999d904c7ddd73d943ffc333
Julie Kim <Julie@Julies-MacBook-Pro-2.local>
2018-04-17 13:17:28 -0400

    `model_select(covariates, responses, cutoff)` fits the linear regression `responses ~ covariates` and retains only those covariates whose coefficient p-values are less than or equal to `cutoff`. It then fits another regression using only the retained covariates and returns the p-values from this reduced model. If there are no retained covariates return an empty vector.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,14 @@ generate_data = function(n, p) {
   vec = rnorm(n)
   return(list("covariates" = mat, "responses" = vec))
 }
+
+model_select = function(covariates, responses, cutoff) {
+  sum.coef = summary(lm(responses ~ covariates))$coefficients
+  p.vals = sum.coef[,"Pr(>|t|)"][-1]
+  cov.i = which(p.vals <= cutoff)
+  if (length(cov.i) == 0) return(cov.i)
+  
+  sum.coef2 = summary(lm(responses ~ covariates[, cov.i]))$coefficients
+  p.vals2 = sum.coef2[,"Pr(>|t|)"][-1]
+  return(p.vals2)
+}
