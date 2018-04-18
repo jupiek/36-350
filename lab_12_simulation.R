26b0c4f189518317855dd85b8e1594b512b64a4e
Julie Kim <Julie@Julies-MacBook-Pro-2.local>
2018-04-18 09:35:32 -0400

    + function `model_select` edited to return the original variable names instead of indices
    + function `run_simulation(n_trials, n, p, cutoff)` added. It uses the previous two functions to run `n_trials` simulations which uses data from `generate_data` in `model_select`, collects the returned p-values and displays a histogram of the p-values.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -12,5 +12,30 @@ model_select = function(covariates, responses, cutoff) {
   
   sum.coef2 = summary(lm(responses ~ covariates[, cov.i]))$coefficients
   p.vals2 = sum.coef2[,"Pr(>|t|)"][-1]
+  names(p.vals2) = names(p.vals)[cov.i]
   return(p.vals2)
 }
+
+run_simulation = function(n_trials, n, p, cutoff) {
+  vals = vector()
+  for (i in 1:n_trials) {
+    gen.dat = generate_data(n, p)
+    pvals = model_select(gen.dat$covariates, gen.dat$responses, cutoff)
+    if (length(pvals) > 0) {
+      for (i in length(pvals))
+        vals[length(vals) + i] = pvals[i]
+    }
+  }
+  if (length(vals) == 0) return(vals)
+  return(hist(vals))
+}
+
+run_simulation(1, 100, 10, 0.05)
+run_simulation(1, 1000, 10, 0.05)
+run_simulation(1, 10000, 10, 0.05)
+run_simulation(1, 100, 20, 0.05)
+run_simulation(1, 1000, 20, 0.05)
+run_simulation(1, 10000, 20, 0.05)
+run_simulation(1, 100, 50, 0.05)
+run_simulation(1, 1000, 50, 0.05)
+run_simulation(1, 10000, 50, 0.05)
